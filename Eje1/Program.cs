4f80e987b15a96b85686d7c12f182aa1b0622ca0
caroldbt <carol123dbt@gmail.com>
2021-06-30 00:00:46 +0200

    Ejercicios

---
+++ Eje1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Eje1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ejercicio 1
+            int a = 15;
+            int b = 8;
+            System.Console.WriteLine("Suma: {0} + {1}", a, b + " = " + (a + b));
+            System.Console.WriteLine("Resta: {0} - {1}", a, b + " = " + (a - b));
+            System.Console.WriteLine("Multiplicación: {0} * {1}", a, b + " = " + (a * b));
+            System.Console.WriteLine("División: {0} / {1}", a, b + " = " + (a / b));
+            System.Console.WriteLine("Resto: {0} % {1}", a, b + " = " + (a % b));
+
+        }
+    }
+}
